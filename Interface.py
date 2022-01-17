5dac68babd37d37df542cf90defd25823b18d56e
BarbsII <barbara.torres.cunha@gmail.com>
2022-01-17 18:01:32 -0300

    Enviando Pelo Pycharm

--- Interface.py
+++ Interface.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # Abertura da Janela
 janela = Tk()
 
@@ -6,13 +7,13 @@ janela.title("LanchoNET") #Título da Página
 
 # MENSAGENS
 boasVindas = Label(janela, text="Boas Vindas! Este é o Sistema LanchoNet")
-boasVindas.grid(column=0, row=0)
+boasVindas.grid(column=0, row=0, padx=10, pady=10)
 
 escolha = Label(janela, text='Escolha sua Ação:')
 escolha.grid(column=0, row=1)
 
 # BOTÕES
-bVender = Button(janela, text='Venda')
+bVender = Button(janela, text='Venda', command=teste)
 bTabela = Button(janela, text='Tabela de Vendas')
 bRegistro = Button(janela, text='Registro Geral de Vendas')
 bEncerrar = Button(janela, text='Encerrar')
@@ -20,7 +21,7 @@ bEncerrar = Button(janela, text='Encerrar')
 botoes = [bVender, bTabela, bRegistro, bEncerrar]
 y = 2
 for botao in botoes:
-    botao.grid(column=0, row=y)
+    botao.grid(column=0, row=y,padx=10, pady=3)
     y += 1
 
 janela.mainloop() # Mantém a janela aberta
